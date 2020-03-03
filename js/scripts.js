50324d5be1217bfb0e9b024abc1c5012730b9e4a
Екатерина Лукинова <katyushta@rambler.ru>
2020-03-03 16:16:03 +0300

    video-page

--- js/scripts.js
+++ js/scripts.js
@@ -106,7 +106,15 @@ $( document ).ready(function() {
     //     return false;
     // });
 
+//video-page
 
+    $('.vid-item').each(function(index){
+        $(this).on('click', function(){
+            var current_index = index+1;
+            $('.vid-item .thumb').removeClass('active');
+            $('.vid-item:nth-child('+current_index+') .thumb').addClass('active');
+        });
+    });
 
 
 });
