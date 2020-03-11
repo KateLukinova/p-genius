aa084ec48c8f1ce298567924a9d1b260b6c3f235
Екатерина Лукинова <katyushta@rambler.ru>
2020-03-11 20:08:49 +0300

    cat

--- js/scripts.js
+++ js/scripts.js
@@ -49,6 +49,12 @@ $( document ).ready(function() {
         $('.best-deal-carousel').slick('slickNext');
     });
 
+    //modal trigger
+    $("#button-card-pay").click(function () {
+        $('#pay-select').modal('hide');
+        $('#pay-card').modal('show');
+    });
+
 
 
     $('.slider-reviews').slick({
@@ -115,32 +121,36 @@ $( document ).ready(function() {
             $('.vid-item:nth-child('+current_index+') .thumb').addClass('active');
         });
     });
+    if (window.location.href.indexOf("index") > -1) {
+        var initialBoardCoordinate = $('#moving-block').offset().top; //изначальная верхняя координата движущегося блока
+        var footerCoordinate = $('#below-block').offset().top; //верх блока, где надо остановиться
+        var footerHeight = $('#below-block').first().offset().top;// от низа сайта до места где остановиться
+        var boardHeight = $('#moving-block').height();
+        var headerHeight = $('#border-block').offset().top;
 
+
+        if (screen.width > 640) {
+            $(window).scroll(function () {
+                var currentCoordinate = $(window).scrollTop();
+
+                if (currentCoordinate >= headerHeight - 280 && currentCoordinate < footerCoordinate - 700) {
+                    $('#moving-block').css('position', 'fixed').css('top', '20vw').css('bottom', 'auto');
+                } else if (currentCoordinate >= footerCoordinate - 1000) {
+                    $('#moving-block').css('position', 'absolute').css('bottom', '0').css('top', 'auto');
+                } else {
+                    $('#moving-block').css('position', 'static');
+                }
+            });
+        }
+
+        $('.jquery-background-video').bgVideo({fadeIn: 2000});
+
+    }
     AOS.init({disable: 'mobile'});
 
-    var initialBoardCoordinate = $('#moving-block').offset().top; //изначальная верхняя координата движущегося блока
-    var footerCoordinate = $('#below-block').offset().top; //верх блока, где надо остановиться
-    var footerHeight = $('#below-block').first().offset().top;// от низа сайта до места где остановиться
-    var boardHeight = $('#moving-block').height();
-    var headerHeight = $('#border-block').offset().top;
 
 
-    if (screen.width > 640) {
-        $(window).scroll(function () {
-            var currentCoordinate = $(window).scrollTop();
 
-            if (currentCoordinate >= headerHeight - 280 && currentCoordinate < footerCoordinate - 700) {
-                $('#moving-block').css('position', 'fixed').css('top', '20vw').css('bottom', 'auto');
-            } else if (currentCoordinate >= footerCoordinate - 1000) {
-                $('#moving-block').css('position', 'absolute').css('bottom', '0').css('top', 'auto');
-            } else {
-                $('#moving-block').css('position', 'static');
-            }
-        });
-    }
-
-
-    $('.jquery-background-video').bgVideo({fadeIn: 2000});
 
 //video-hover
     var figure = $(".video-wrapper").hover( hoverVideo, hideVideo );
@@ -152,6 +162,116 @@ $( document ).ready(function() {
     function hideVideo(e) {
         $('video', this).get(0).pause();
     }
+
+    // dropdown-user
+    $('.user-block').click(function () {
+        $(this).find('.user-dropdown-content').toggleClass('is-active')
+    })
 });
 
+//cradit card mask
 
+let ccNumberInput = document.querySelector('.cc-number-input'),
+    ccNumberPattern = /^\d{0,16}$/g,
+    ccNumberSeparator = " ",
+    ccNumberInputOldValue,
+    ccNumberInputOldCursor,
+
+    ccExpiryInput = document.querySelector('.cc-expiry-input'),
+    ccExpiryPattern = /^\d{0,4}$/g,
+    ccExpirySeparator = "/",
+    ccExpiryInputOldValue,
+    ccExpiryInputOldCursor,
+
+    ccCVCInput = document.querySelector('.cc-cvc-input'),
+    ccCVCPattern = /^\d{0,3}$/g,
+
+    mask = (value, limit, separator) => {
+        var output = [];
+        for (let i = 0; i < value.length; i++) {
+            if ( i !== 0 && i % limit === 0) {
+                output.push(separator);
+            }
+
+            output.push(value[i]);
+        }
+
+        return output.join("");
+    },
+    unmask = (value) => value.replace(/[^\d]/g, ''),
+    checkSeparator = (position, interval) => Math.floor(position / (interval + 1)),
+    ccNumberInputKeyDownHandler = (e) => {
+        let el = e.target;
+        ccNumberInputOldValue = el.value;
+        ccNumberInputOldCursor = el.selectionEnd;
+    },
+    ccNumberInputInputHandler = (e) => {
+        let el = e.target,
+            newValue = unmask(el.value),
+            newCursorPosition;
+
+        if ( newValue.match(ccNumberPattern) ) {
+            newValue = mask(newValue, 4, ccNumberSeparator);
+
+            newCursorPosition =
+                ccNumberInputOldCursor - checkSeparator(ccNumberInputOldCursor, 4) +
+                checkSeparator(ccNumberInputOldCursor + (newValue.length - ccNumberInputOldValue.length), 4) +
+                (unmask(newValue).length - unmask(ccNumberInputOldValue).length);
+
+            el.value = (newValue !== "") ? newValue : "";
+        } else {
+            el.value = ccNumberInputOldValue;
+            newCursorPosition = ccNumberInputOldCursor;
+        }
+
+        el.setSelectionRange(newCursorPosition, newCursorPosition);
+
+        highlightCC(el.value);
+    },
+    highlightCC = (ccValue) => {
+        let ccCardType = '',
+            ccCardTypePatterns = {
+                amex: /^3/,
+                visa: /^4/,
+                mastercard: /^5/,
+                disc: /^6/,
+
+                genric: /(^1|^2|^7|^8|^9|^0)/,
+            };
+
+        for (const cardType in ccCardTypePatterns) {
+            if ( ccCardTypePatterns[cardType].test(ccValue) ) {
+                ccCardType = cardType;
+                break;
+            }
+        }
+
+        let activeCC = document.querySelector('.cc-types__img--active'),
+            newActiveCC = document.querySelector(`.cc-types__img--${ccCardType}`);
+
+        if (activeCC) activeCC.classList.remove('cc-types__img--active');
+        if (newActiveCC) newActiveCC.classList.add('cc-types__img--active');
+    },
+    ccExpiryInputKeyDownHandler = (e) => {
+        let el = e.target;
+        ccExpiryInputOldValue = el.value;
+        ccExpiryInputOldCursor = el.selectionEnd;
+    },
+    ccExpiryInputInputHandler = (e) => {
+        let el = e.target,
+            newValue = el.value;
+
+        newValue = unmask(newValue);
+        if ( newValue.match(ccExpiryPattern) ) {
+            newValue = mask(newValue, 2, ccExpirySeparator);
+            el.value = newValue;
+        } else {
+            el.value = ccExpiryInputOldValue;
+        }
+    };
+
+ccNumberInput.addEventListener('keydown', ccNumberInputKeyDownHandler);
+ccNumberInput.addEventListener('input', ccNumberInputInputHandler);
+
+ccExpiryInput.addEventListener('keydown', ccExpiryInputKeyDownHandler);
+ccExpiryInput.addEventListener('input', ccExpiryInputInputHandler);
